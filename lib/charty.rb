1004652535bcd31e44c1683bdeec1aa2729aedd5
284km <k.furuhashi10@gmail.com>
2019-03-12 20:47:56 +0900

    Remove alias Rdv and RdvMain

--- lib/charty.rb
+++ lib/charty.rb
@@ -5,9 +5,6 @@ require_relative "charty/layout"
 require_relative "charty/linspace"
 require_relative "charty/table"
 
-Rdv = Charty
-RdvMain = Rdv::Main
-
 module Charty
   def self.new(*args)
     Charty::Main.new(*args)
