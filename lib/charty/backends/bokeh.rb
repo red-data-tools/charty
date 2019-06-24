3b460519b8311a006ee49d277f03b132e1a6f013
kantarow <kanta7306@gmail.com>
2019-06-24 23:09:08 +0900

    Add some graphs

--- lib/charty/backends/bokeh.rb
+++ lib/charty/backends/bokeh.rb
@@ -13,14 +13,57 @@ module Charty
     end
 
     def render(context, filename)
+      plot = plot(context)
+      save(plot, context, filename)
+      ::Bokeh::IO::show(plot)
+    end
+
+    def save(plot, context, filename)
+      if filename
+        ::Bokeh::IO::save(plot, filename)
+      end
+    end
+
+    def plot(context)
+      #TODO To implement boxplot, bublle, error_bar, hist.
+      
       plot = @plot.figure(title: context&.title)
       case context.method
+        when :bar
+          context.series.each do |data|
+            plot.vbar(data.xs.to_a, nil, data.ys.to_a)
+          end
+
+        when :barh
+          context.series.each do |data|
+            context.series.each do |data|
+              plot.hbar(data.xs.to_a, nil, data.ys.to_a)
+            end
+          end
+
+        when :boxplot
+          raise NotImplementedError
+
+        when :bubble
+          raise NotImplementedError
+
         when :curve
           context.series.each do |data|
             plot.line(data.xs.to_a, data.ys.to_a)
           end
+
+        when :scatter
+          context.series.each do |data|
+            plot.scatter(data.xs.to_a, data.ys.to_a)
+          end
+
+        when :error_bar
+          raise NotImplementedError
+
+        when :hist
+          raise NotImplementedError
       end
-      ::Bokeh::IO::show(plot)
+      plot
     end
   end
 end
