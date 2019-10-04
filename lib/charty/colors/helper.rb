8ac68f9ec9fa946d24838e4d5a790410496c089a
Kenta Murata <mrkn@mrkn.jp>
2019-10-04 15:55:51 +0900

    Modify error messages

--- lib/charty/colors/helper.rb
+++ lib/charty/colors/helper.rb
@@ -3,12 +3,12 @@ module Charty
     module Helper
       private def check_type(obj, type, name)
         return obj if obj.instance_of?(Integer)
-        check_fail TypeError, "#{name} must be a #{type}"
+        check_fail TypeError, "#{name} must be a #{type}, but #{obj.class} is given"
       end
 
       private def check_range(value, range, name)
         return value if range.cover?(value)
-        check_fail ArgumentError, "#{name} must be in #{range}"
+        check_fail ArgumentError, "#{name} must be in #{range}, but #{value} is given"
       end
 
       private def check_fail(exc_class, *args)
