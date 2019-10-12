07e3878f5678e024ddec9e03bb8506e24aa6dc63
Kenta Murata <mrkn@mrkn.jp>
2019-10-13 00:04:40 +0900

    Remove lowercase aliases of color factories

--- lib/charty/color_methods.rb
+++ lib/charty/color_methods.rb
@@ -19,8 +19,6 @@ module Charty
       end
     end
 
-    alias rgb RGB
-
     def RGBA(*args)
       case args.length
       when 1..2
@@ -48,8 +46,6 @@ module Charty
       end
     end
 
-    alias rgba RGBA
-
     def HSL(*args)
       case args.length
       when 1
@@ -68,8 +64,6 @@ module Charty
               "wrong number of arguments (given #{args.length}, expected 1 or 3)"
       end
     end
-
-    alias hsl HSL
 
     def HSLA(*args)
       case args.length
@@ -97,8 +91,6 @@ module Charty
               "wrong number of arguments (given #{args.length}, expected 1, 2, or 4)"
       end
     end
-
-    alias hsla HSLA
   end
 
   extend ColorMethods
