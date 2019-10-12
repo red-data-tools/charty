78d6c585c45d925424d72bcad4e73a5b5d5f631c
Kenta Murata <mrkn@mrkn.jp>
2019-10-13 02:32:56 +0900

    Rewrite named color mapping as a class

--- lib/charty/colors/named_colors.rb
+++ lib/charty/colors/named_colors.rb
@@ -3,34 +3,85 @@ require_relative 'color_data'
 module Charty
   module Colors
     module NamedColors
-      module ColorMapping
-        def self.extended(obj)
-          obj.instance_variable_set(:@cache, {})
+      class Mapping
+        def initialize
+          @mapping = {}
+          @cache = {}
         end
 
         attr_reader :cache
 
-        def []=(key, value)
-          super
+        def [](name)
+          if NamedColors.nth_color?(name)
+            cycle = ColorData::DEFAULT_COLOR_CYCLE
+            name = cycle[name[1..-1].to_i % cycle.length]
+          end
+          if cache.has_key?(name)
+            cache[name]
+          else
+            cache[name] = lookup_no_color_cycle(name)
+          end
+        end
+
+        private def lookup_no_color_cycle(color)
+          orig_color = color
+          case color
+          when /\Anone\z/i
+            return RGBA.new(0, 0, 0, 0)
+          when String
+            # nothing to do
+          when Symbol
+            color = color.to_s
+          else
+            color = color.to_str
+          end
+          color = @mapping.fetch(color, color)
+          case color
+          when /\A#\h+\z/
+            case color.length - 1
+            when 3, 6
+              RGB.from_hex_string(color)
+            when 4, 8
+              RGBA.from_hex_string(color)
+            else
+              raise RuntimeError,
+                    "[BUG] Invalid hex string form #{color.inspect} for #{name.inspect}"
+            end
+          when Array
+            case color.length
+            when 3
+              RGB.new(*color)
+            when 4
+              RGBA.new(*color)
+            else
+              raise RuntimeError,
+                    "[BUG] Invalid number of color components #{color} for #{name.inspect}"
+            end
+          else
+            color
+          end
+        end
+
+        def []=(name, value)
+          @mapping[name] = value
         ensure
           cache.clear
         end
 
-        def delete(key)
-          super
+        def delete(name)
+          @mapping.delete(name)
         ensure
           cache.clear
         end
 
         def update(other)
-          super
+          @mapping.update(other)
         ensure
           cache.clear
         end
       end
 
-      MAPPING = {}
-      MAPPING.extend(ColorMapping)
+      MAPPING = Mapping.new
       MAPPING.update(ColorData::XKCD_COLORS)
       ColorData::XKCD_COLORS.each do |key, value|
         MAPPING[key.sub("grey", "gray")] = value if key.include? "grey"
@@ -43,56 +94,7 @@ module Charty
       MAPPING.update(ColorData::BASE_COLORS)
 
       def self.[](name)
-        if nth_color?(name)
-          cycle = ColorData::DEFAULT_COLOR_CYCLE
-          name = cycle[name[1..-1].to_i % cycle.length]
-        end
-        if MAPPING.cache.has_key?(name)
-          color = MAPPING.cache[name]
-        else
-          color = lookup_no_color_cycle(name)
-          MAPPING.cache[name] = color
-        end
-        color
-      end
-
-      def self.lookup_no_color_cycle(color)
-        orig_color = color
-        case color
-        when /\Anone\z/i
-          return RGBA.new(0, 0, 0, 0)
-        when String
-          # nothing to do
-        when Symbol
-          color = color.to_s
-        else
-          color = color.to_str
-        end
-        color = MAPPING.fetch(color, color)
-        case color
-        when /\A#\h+\z/
-          case color.length - 1
-          when 3, 6
-            RGB.from_hex_string(color)
-          when 4, 8
-            RGBA.from_hex_string(color)
-          else
-            raise RuntimeError,
-                  "[BUG] Invalid hex string form #{color.inspect} for #{name.inspect}"
-          end
-        when Array
-          case color.length
-          when 3
-            RGB.new(*color)
-          when 4
-            RGBA.new(*color)
-          else
-            raise RuntimeError,
-                  "[BUG] Invalid number of color components #{color} for #{name.inspect}"
-          end
-        else
-          color
-        end
+        MAPPING[name]
       end
 
       # Return whether `name` is an item in the color cycle.
