c6e7a35622d4f29005b996b75ff3a442c3649646
Kenta Murata <mrkn@mrkn.jp>
2021-04-03 12:03:09 +0900

    Stop using positional arguments

--- lib/charty/plot_methods.rb
+++ lib/charty/plot_methods.rb
@@ -6,11 +6,11 @@ module Charty
     # @param y
     # @param color
     # @param data
-    def bar_plot(x=nil, y=nil, color=nil, **options, &block)
+    def bar_plot(x: nil, y: nil, color: nil, **options, &block)
       Plotters::BarPlotter.new(x, y, color, **options, &block)
     end
 
-    def box_plot(x=nil, y=nil, color=nil, **options, &block)
+    def box_plot(x: nil, y: nil, color: nil, **options, &block)
       Plotters::BoxPlotter.new(x, y, color, **options, &block)
     end
   end
