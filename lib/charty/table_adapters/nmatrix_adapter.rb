23dcd92a651084d560624b8bab14d919aa40b9ad
Kenta Murata <mrkn@mrkn.jp>
2021-05-13 12:11:54 +0900

    Fix nmatrix table adapter

--- lib/charty/table_adapters/nmatrix_adapter.rb
+++ lib/charty/table_adapters/nmatrix_adapter.rb
@@ -1,6 +1,6 @@
 module Charty
   module TableAdapters
-    class NMatrixAdapter
+    class NMatrixAdapter < BaseAdapter
       TableAdapters.register(:nmatrix, self)
 
       def self.supported?(data)
@@ -17,24 +17,33 @@ module Charty
           raise ArgumentError, "Unsupported data format"
         end
         @data = data
-        @column_names = generate_column_names(data.shape[1], columns)
+        self.columns = Index.new(generate_column_names(data.shape[1], columns))
+        self.index = index || RangeIndex.new(0 ... length)
       end
 
-      attr_reader :column_names, :data
+      attr_reader :data
+
+      def length
+        data.shape[0]
+      end
+
+      def column_length
+        data.shape[1]
+      end
 
       def [](row, column)
         if row
           @data[row, resolve_column_index(column)]
         else
-          Charty::Vector.new(@data[:*, resolve_column_index(column)].reshape([@data.shape[0]]),
-                             name: column)
+          column_data = @data[:*, resolve_column_index(column)].reshape([@data.shape[0]])
+          Charty::Vector.new(column_data, index: index, name: column)
         end
       end
 
       private def resolve_column_index(column)
         case column
         when String, Symbol
-          index = column_names.index(column.to_s)
+          index = column_names.index(column.to_sym) || column_names.index(column.to_s)
           return index if index
           raise IndexError, "invalid column name: #{column}"
         when Integer
