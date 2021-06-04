43d1a4ec28cbd7502050236f45c903f66179e7a1
Kenta Murata <mrkn@mrkn.jp>
2021-06-05 02:31:51 +0900

    test: Insert pandas_required

--- test/table/table_group_by_test.rb
+++ test/table/table_group_by_test.rb
@@ -190,6 +190,8 @@ class TableGroupByTest < Test::Unit::TestCase
   end
 
   def setup_table_by_pandas
+    pandas_required
+
     @data = Pandas::DataFrame.new(data: @data)
     @table = Charty::Table.new(@data)
 
