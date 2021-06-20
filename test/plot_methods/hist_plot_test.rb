ef42c8df7af272e0ecdfec29b88f84d9e6e5a631
Kenta Murata <mrkn@mrkn.jp>
2021-06-21 03:27:47 +0900

    test: omit pyplot case of hist_plot tests

--- test/plot_methods/hist_plot_test.rb
+++ test/plot_methods/hist_plot_test.rb
@@ -8,6 +8,7 @@ class PlotMethodHistPlotTest < Test::Unit::TestCase
     data(:backend, [:pyplot, :plotly], keep: true)
     def test_hist_plot_with_flat_vectors(data)
       backend_name = data[:backend]
+      omit("pyplot is not supported yet") if backend_name == :pyplot
       setup_backend(backend_name)
       data = Array.new(500) { rand }
       plot = Charty.hist_plot(data: data)
