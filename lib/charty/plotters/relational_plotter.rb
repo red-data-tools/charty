7c4fd8261866c7d17a5b0f573d24d950445c2a1e
Kenta Murata <mrkn@mrkn.jp>
2021-06-29 10:10:07 +0900

    Add TODO comment for the future work

--- lib/charty/plotters/relational_plotter.rb
+++ lib/charty/plotters/relational_plotter.rb
@@ -611,6 +611,7 @@ module Charty
 
         @plot_data = Charty::Table.new(plot_data)
         @variables = variables.select do |var, name|
+          # TODO: this overwrite the cached vector in the original data
           @plot_data[var].notnull.any?
         end
       end
